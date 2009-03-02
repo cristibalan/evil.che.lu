2179ffca7176265cb171d216359f78a634e021fc
Cristi Balan <evil@che.lu>
2009-03-02 14:20:09 +0100

    duck punch url_for instead of hacking webby guts

---
+++ lib/hack_url_for.rb
@@ -0,0 +1,21 @@
+# TODO send url_for patch to webby
+module Webby::Helpers
+  module UrlHelper
+    def url_for( *args )
+      opts = Hash === args.last ? args.pop : {}
+      obj = args.first
+
+      anchor = opts.delete(:anchor)
+      escape = opts.has_key?(:escape) ? opts.delete(:escape) : true
+
+      url = Webby::Resources::Resource === obj ? obj.url : obj.to_s
+      url = escape_once(url) if escape
+      url << "#" << anchor if anchor
+
+      # hack hack
+      p "!!!!!!!!!"
+      url.gsub!(/\.html$/, "") if url[0].chr == "/"
+      return url
+    end
+  end
+end
